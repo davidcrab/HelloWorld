29ba35791cc87e2fca51347bf8d400c4b1140f03
davidcrab <davidcrabtree18@icloud.com>
2024-08-08 16:37:56 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by David Crabtree on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
